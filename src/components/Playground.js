3f507a313bd89f60d52d8406944e5756a06b632b
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-07 12:41:35 -0600

    wip

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -15,6 +15,7 @@ export default function Playground(props) {
   return (
     <div className="container">
       <h3>Playground for Web {props.cohort}</h3>
+      <div>the count is {count}</div>
     </div>
   );
 }
