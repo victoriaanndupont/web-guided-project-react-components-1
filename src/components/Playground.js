24eb2e43d0a6c4d08ecac9b3a01e52ea7f423421
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-07 12:59:26 -0600

    wip

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -27,6 +27,7 @@ export default function Playground(props) {
     <div className="container">
       <h3>Playground for Web {props.cohort}</h3>
       <div>the count is {count}</div>
+      <div>the current weapon is scissors</div>
       <button
         onClick={(event) => {
           setCount(count + 1);
@@ -34,6 +35,9 @@ export default function Playground(props) {
       >
         increase
       </button>
+      <button>pick scissors</button>
+      <button>pick rock</button>
+      <button>pick paper</button>
     </div>
   );
 }
